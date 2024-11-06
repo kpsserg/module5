e4f45949b90bac0115c8e65b6a21c57119078a8b
kpsserg <kpsserg@gmail.com>
2024-11-07 00:11:59 +0300

    HomeWork - module_5_1.py

--- .venv/module5_lesson_1.py
+++ .venv/module5_lesson_1.py
@@ -1,6 +1,21 @@
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.say_info()
+
+    def say_info(self):
+        print(f'Привет, меня зовут {self.name}, мне {self.age}')
+
+    def birthday(self):
+        self.age += 1
+        print(f'Я {self.name}. У меня день рождения. Мне теперь {self.age}')
 
 
+den = Human('Денис', 22)
+max = Human('Максим', 22)
 
-print('Lesson 5.1')
+den.surname = 'Попов'
+print(den.surname)
 
-print('hi')
+den.birthday()
